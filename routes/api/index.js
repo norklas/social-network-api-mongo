b88ce25552bb9569c38ae9ae617df0ada64f11ec
Nick Melanson <nickm890@gmail.com>
2022-05-29 21:33:16 -0400

    added thought routes, and updated index.js in api folder to reflect the new routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
 // Connecting routes to the server
 const router = require("express").Router();
 const userRoutes = require("./user-routes");
+const thoughtRoutes = require("./thought-routes");
 
 // Add prefix
 router.use("/users", userRoutes);
+router.use("/thoughts", thoughtRoutes);
 
 module.exports = router;
